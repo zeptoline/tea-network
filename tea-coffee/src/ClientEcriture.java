30d5916b44b25438fd4f2c9125c380c8ef12f2d0
Cédric Berland <zeptoline2@gmail.com>
2016-02-25 18:54:05 +0100

    Connexion client (& cleanup)

--- tea-coffee/src/ClientEcriture.java
+++ tea-coffee/src/ClientEcriture.java
@@ -13,20 +13,20 @@ import java.util.Scanner;
 public class ClientEcriture {
 
 	public static void main(String[] args) {
-		
+
 		//Faire un hashmap pour la finger table
 		//+ variable pour connaître son hash
 		//+ variable pour connaître son prédécesseur
-		
+
 		String hash = "";
 		String IPsuccesseur = "";
 		String IPpredecesseur = "";
-		
+
 		String ip = "";
 		try (Socket hashSocket = new Socket("localhost", 8001);
-		BufferedReader hashEntree = new BufferedReader(new InputStreamReader (hashSocket.getInputStream()));
-		PrintWriter hashSortie = new PrintWriter (hashSocket.getOutputStream(), true);) {
-			ip = "127.0.0.1";
+				BufferedReader hashEntree = new BufferedReader(new InputStreamReader (hashSocket.getInputStream()));
+				PrintWriter hashSortie = new PrintWriter (hashSocket.getOutputStream(), true);) {
+			ip = InetAddress.getLocalHost().getHostAddress();
 			hashSortie.println(ip);
 			hash = hashEntree.readLine();
 		} catch(UnknownHostException e) {
@@ -34,35 +34,35 @@ public class ClientEcriture {
 		} catch ( IOException e ) {
 			System.out.println(e.getMessage());
 		}
-		
-		
+
+
 		Socket WSsock = null;
 		boolean estPremier = false;
 		try {
 			WSsock = new Socket("localhost" , 8000);
-		
-		InputStream WSfluxEntree = WSsock.getInputStream();
-		OutputStream WSfluxSortie = WSsock.getOutputStream ( );
 
-		BufferedReader entree = new BufferedReader(new InputStreamReader (WSfluxEntree));
-		PrintWriter sortie = new PrintWriter (WSfluxSortie, true);
-		
-		String connec = "yo:" + hash + ":" + ip;
-		sortie.println(connec);
-		
-		String entreeLue = entree.readLine();
-		if(entreeLue.equals("yaf")) {
-			System.out.println("Vous êtes le premier client sur le réseau");
-			IPsuccesseur = hash;
-			IPpredecesseur = hash;
-			estPremier = true;
-		}
-		else if(!entreeLue.equals("wrq")) {
-			System.out.println("votre successeur a l'addresse " + entreeLue);
-			IPsuccesseur = entreeLue;
-		}
-		else
-			System.out.println("La connection est un échec");
+			InputStream WSfluxEntree = WSsock.getInputStream();
+			OutputStream WSfluxSortie = WSsock.getOutputStream ( );
+
+			BufferedReader entree = new BufferedReader(new InputStreamReader (WSfluxEntree));
+			PrintWriter sortie = new PrintWriter (WSfluxSortie, true);
+
+			String connec = "yo:" + hash + ":" + ip;
+			sortie.println(connec);
+
+			String entreeLue = entree.readLine();
+			if(entreeLue.equals("yaf")) {
+				System.out.println("Vous êtes le premier client sur le réseau");
+				IPsuccesseur = hash;
+				IPpredecesseur = hash;
+				estPremier = true;
+			}
+			else if(!entreeLue.equals("wrq")) {
+				System.out.println("votre successeur a l'addresse " + entreeLue);
+				IPsuccesseur = entreeLue;
+			}
+			else
+				System.out.println("La connection est un échec");
 
 		} catch(UnknownHostException e) {
 			System.out.println(e.getMessage());
@@ -70,26 +70,26 @@ public class ClientEcriture {
 			System.out.println(e.getMessage());
 		}
 
-		}
-		
-		
-		//Le client lecteur va écouter tout le monde sur un port prédéfinis commun
-		//(par exemple le port 2016)
-		//Il devra différencier les messages du successeur
-		//Et les messages des nouveaux arrivants
-		
-		Thread myThreadLecture = new Thread(new ClientLecture(WSsock));
-		myThreadLecture.start();
-		
-		Scanner sc = new Scanner(System.in);
-		String str = "";
-		while (true) {
-			System.out.println("Message a envoyer : ");
-			str = sc.nextLine();
-			sortie.println(str);
-		}
+	}
 
-		
-	
+
+	//Le client lecteur va écouter tout le monde sur un port prédéfinis commun
+	//(par exemple le port 2016)
+	//Il devra différencier les messages du successeur
+	//Et les messages des nouveaux arrivants
+/*
+	Thread myThreadLecture = new Thread(new ClientLecture(WSsock));
+	myThreadLecture.start();
+
+	Scanner sc = new Scanner(System.in);
+	String str = "";
+	while (true) {
+		System.out.println("Message a envoyer : ");
+		str = sc.nextLine();
+		sortie.println(str);
+	}
+*/
+
+
 
 }
---
+++ tea-coffee/src/app/Peers.java
@@ -0,0 +1,215 @@
+package app;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Peers {
+	private static final String IP_SERVEUR = "localhost";
+
+	private static String hash ="";
+
+	private static String IPsuccesseur = "";
+	private static int idSuccesseur;
+	private static String IPpredecesseur = "";
+	private static int idPredecesseur;
+
+	private static String ip;
+
+	public static void main(String[] args) {
+
+		try {
+			ip = InetAddress.getLocalHost().getHostAddress();
+		} catch (UnknownHostException e1) {System.out.println("Can't find ip"); return;}
+
+
+		try 
+		(		Socket hashSocket = new Socket(IP_SERVEUR, 8001);
+				BufferedReader hashEntree = new BufferedReader(new InputStreamReader (hashSocket.getInputStream()));
+				PrintStream hashSortie = new PrintStream (hashSocket.getOutputStream(), true);)
+		{
+			hashSortie.println(ip);
+			hash = hashEntree.readLine();
+		} 	
+		catch(UnknownHostException e) {System.out.println("hôte inconnu"); return;}
+		catch ( IOException e ) {System.out.println("erreur I/O"); return;}
+
+
+		try (Socket WSsock = new Socket (IP_SERVEUR, 8000);
+				BufferedReader entree = new BufferedReader(new InputStreamReader (WSsock.getInputStream()));
+				PrintStream sortie = new PrintStream (WSsock.getOutputStream(), true);
+				)
+		{
+			String connec = "yo:"+hash+":"+ip;
+			sortie.println(connec);
+
+			String entreeLue = entree.readLine();
+			if(entreeLue.equals("yaf")) { 
+				System.out.println("Vous êtes le premier client sur le réseau");
+				IPsuccesseur = ip;
+				idSuccesseur = Integer.valueOf(hash);
+				IPpredecesseur = ip;
+				idPredecesseur = Integer.valueOf(hash);
+			}
+			else if(!entreeLue.equals("wrq")) {
+				System.out.println("votre successeur à l'addresse " + entreeLue);
+				ClientJoining(entreeLue);
+
+			}
+			else
+				System.out.println("La connection est un échec");
+
+		} 	
+		catch(UnknownHostException e) {System.out.println("hôte inconnu"); return;}
+		catch ( IOException e ) {System.out.println("erreur I/O"); return;}
+
+
+		Thread te = new Thread(new Runnable() {
+			public void run() {
+				try (ServerSocket ss = new ServerSocket(2016);){
+					while(true) {
+						try(Socket cs = ss.accept();
+								BufferedReader d = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+								PrintStream os = new PrintStream (cs.getOutputStream(), true);
+								) 
+						{
+							String message = "";
+							while((message = d.readLine())!= null) 
+							{
+								TreatMessage(message, os, d, cs);
+								
+							}
+						}
+						catch (IOException e) {
+							System.out.println("Serveur d'écoute IO Erreur");
+						}
+					}
+
+				} catch (IOException e) {}
+			}
+		});
+		te.start();
+
+
+
+
+	}
+
+
+	private static void TreatMessage(String message, PrintStream os, BufferedReader d, Socket cs) {
+		String[] cmds = message.split(":");
+		int key;
+		switch (cmds[0]) {
+		case "addme_pls" :
+			AddToNetwork(os, d, cs);
+			break;
+		case "lookin'for_someone" :
+			
+			break;
+		case "I'm leaving" :
+			System.out.println("fuck, he's leaving");
+			break;
+		case "Hey Im new" :
+			setSuccessor(os, d, cs);
+			break;
+		case "findPred" :
+			 key= Integer.valueOf(cmds[1]);
+			os.println(findPredecessor(key));
+			break;
+		case "findSucc" :
+			key = Integer.valueOf(cmds[1]);
+			os.println(findSuccessor(key));
+			break;
+		default:
+			break;
+		}
+	}
+
+
+	private static void AddToNetwork(PrintStream os, BufferedReader d, Socket cs) {
+		os.println("K.");
+		os.println(IPpredecesseur);
+		IPpredecesseur = cs.getInetAddress().getHostAddress();
+		os.println("hash pls?");
+		try {
+			idPredecesseur = Integer.valueOf(d.readLine());
+		} catch (IOException e) {
+			System.out.println("fuck he's ded");
+		}
+	}
+
+	private static void setSuccessor(PrintStream os, BufferedReader d, Socket cs) {
+		IPsuccesseur = cs.getInetAddress().getHostAddress();
+		os.println("hash pls?");
+		try {
+			idSuccesseur = Integer.valueOf(d.readLine());
+		} catch (IOException e) {
+			System.out.println("fuck he's ded");
+		}
+	}
+	
+	
+	private static void ClientJoining(String IPKnown) {
+
+		IPsuccesseur = IPKnown;
+		
+		try 
+		(		Socket clientPresent = new Socket(IPKnown, 8001);
+				BufferedReader in = new BufferedReader(new InputStreamReader (clientPresent.getInputStream()));
+				PrintStream out = new PrintStream (clientPresent.getOutputStream(), true);)
+		{
+
+			out.println("addme_pls");
+			if(in.readLine().equals("K.")) {
+				IPpredecesseur = in.readLine();
+			}
+			if(in.readLine().equals("hash pls?")) {
+				out.println(hash);
+			}
+			
+		} 	
+		
+		catch(UnknownHostException e) {System.out.println("hôte inconnu"); return;}
+		catch ( IOException e ) {System.out.println("erreur I/O"); return;}
+		
+		try 
+		(		Socket Precedant = new Socket(IPKnown, 8001);
+				BufferedReader in = new BufferedReader(new InputStreamReader (Precedant.getInputStream()));
+				PrintStream out = new PrintStream (Precedant.getOutputStream(), true);)
+		{
+
+			out.println("Hey Im new");
+			if(in.readLine().equals("hash pls?")) {
+				out.println(hash);				
+			}
+		} 	
+		catch(UnknownHostException e) {System.out.println("hôte inconnu"); return;}
+		catch ( IOException e ) {System.out.println("erreur I/O"); return;}
+	}
+	
+	
+	
+	
+	
+	
+	private static String findPredecessor(int key) {
+		// Methode recursive cherchant le predecesseur à une certaine clé
+		System.out.println(IPpredecesseur);
+		System.out.println(idPredecesseur);
+		
+		return "";
+	}
+	private static String findSuccessor(int key) {
+		// Methode recursive cherchant le successeur à une certaine clé
+		
+		//Il faudra parcourir la table des fingers (si on la fait) et renvoyé cette fonction au client le plus proche
+		System.out.println(IPsuccesseur);
+		System.out.println(idSuccesseur);
+		return "";		
+	}
+}
