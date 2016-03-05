f321919bb1537789600037313249cdfb33b763bc
Cédric Berland <zeptoline2@gmail.com>
2016-03-05 01:03:01 +0100

    refreshing routing table every 5 min

--- tea-coffee/src/app/Peers.java
+++ tea-coffee/src/app/Peers.java
@@ -13,16 +13,18 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Peers {
-	private static final String IP_SERVEUR = "172.21.65.13";
+	private static final String IP_SERVEUR = "192.168.0.10";
 	private static final int SERVER_SIZE = 100;
 
 	private static int hash = -1;
 
 	private static HashMap<Integer, String> finger = new HashMap<Integer, String>();
+	private static ArrayList<String> finger_moniteur = new ArrayList<String>();
 	private static String IPsuccesseur = "";
 	private static int idSuccesseur;
 	private static String IPpredecesseur = "";
 	private static int idPredecesseur;
+
 
 	private static String ip;
 
@@ -49,10 +51,10 @@ public class Peers {
 		(		Socket hashSocket = new Socket(IP_SERVEUR, 8001);
 				BufferedReader hashEntree = new BufferedReader(new InputStreamReader (hashSocket.getInputStream()));
 				PrintStream hashSortie = new PrintStream (hashSocket.getOutputStream(), true);)
-				{
+		{
 			hashSortie.println(ip);
 			hash = Integer.valueOf(hashEntree.readLine());
-				} 	
+		} 	
 		catch(UnknownHostException e) {System.err.println("unknown host"); return;}
 		catch ( IOException e ) {System.err.println("erreur I/O HashServer"); return;}
 
@@ -61,24 +63,29 @@ public class Peers {
 				BufferedReader entree = new BufferedReader(new InputStreamReader (WSsock.getInputStream()));
 				PrintStream sortie = new PrintStream (WSsock.getOutputStream(), true);
 				)
-				{
+		{
 			String connec = "yo:"+hash+":"+ip;
 			sortie.println(connec);
 
 			String entreeLue = entree.readLine();
 			if(entreeLue.equals("yaf")) { 
-				System.out.println("You're the first on the network");
+				System.out.println("You're first on the network");
 				IPsuccesseur = ip;
 				idSuccesseur = hash;
 				finger.put(hash, ip);
+				finger_moniteur.add(hash+":"+hash+":"+ip);
 				IPpredecesseur = ip;
 				idPredecesseur = hash;
 				System.out.println("Your hash : "+hash);
 				System.out.println("Your ip : "+ip);
 			}
 			else if(!entreeLue.equals("wrq")) {
-				System.out.println("Your successor is at " + entreeLue);
-				ClientJoining(entreeLue);
+				System.out.println("Joining network...");
+				if(!ClientJoining(entreeLue)){
+					System.err.println("Client cannot join network");
+					return;
+				}
+				System.out.println("Network joined");
 				System.out.println("Your hash : "+hash);
 				System.out.println("Your ip : "+ip);
 
@@ -87,335 +94,294 @@ public class Peers {
 				System.err.println("The connexion failed");
 				return;
 			}
-				} 	
+		} 	
 		catch(UnknownHostException e) {System.err.println("unknown host"); return;}
 		catch ( IOException e ) {System.err.println("erreur I/O Welcome Server"); return;}
 
 
 		Thread te = new Thread(new Runnable() {
 			public void run() {
-				try (ServerSocket ss = new ServerSocket(2016);){
-					System.out.println("Start listening server");
-					while(true) {
-						try(Socket cs = ss.accept();
-								BufferedReader d = new BufferedReader(new InputStreamReader(cs.getInputStream()));
-								PrintStream os = new PrintStream (cs.getOutputStream(), true);
-								) 
-								{
-							String message = "";
-							while((message = d.readLine())!= null) 
-							{
-								TreatMessage(message, os, d, cs);
-
-							}
-								}
-						catch (IOException e) {
-							System.err.println("Listening server IO Erreur");
-						}
-					}
-
-				} catch (IOException e) {}
+				serverListener();
 			}
 		});
 		te.start();
 
+		Thread monitor = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				monitorListener();
+			}
+		});
+		monitor.start();
+
+		Thread refresher = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while(true) {
+					try {
+						Thread.sleep(300000);
+						refreshFinger();
+					} catch (InterruptedException e) {
+						System.err.println("error stopping thread");
+					}
+				}
+
+			}
+		});
+		refresher.start();
+
+		scanCommandLine();
+
+	}
+
+
+
+
+
+
+	//message format :
+	//[command]:[hashTo]:[hashHrom]:[IPFrom]:[message]
+	private static void TreatMessage(String message, PrintStream os, BufferedReader d, Socket cs) {
+		String[] cmds = message.split(":");
+		switch (cmds[0]) {
+		/*
+		 * Init messages
+		 */
+		case "addme":			
+			sendResponse(cmds[3], idPredecesseur+"-"+IPpredecesseur);
+
+			IPpredecesseur=cmds[3];
+			idPredecesseur = PeersUtility.safeParseInt(cmds[2]);
+
+			System.out.println("New Peer added to the network");
+			break;
+
+		case "newSucc" :
+			IPsuccesseur = cmds[3];
+			idSuccesseur = PeersUtility.safeParseInt(cmds[2]);
+			finger.put(idSuccesseur, IPsuccesseur);
+			finger_moniteur.add(hash+":"+idSuccesseur+":"+IPsuccesseur);
+			System.out.println("Successor has been updated - new Successor : "+idSuccesseur);
+			break;
+
+		case "getSucc":
+			int hashToGet = Integer.valueOf(cmds[1]);
+			if(idSuccesseur > hash && hashToGet < idSuccesseur)
+				sendResponse(cmds[3], idSuccesseur+"-"+IPsuccesseur);
+			else if(idSuccesseur < hash && (hashToGet > hash || (hashToGet > 0 && hashToGet < idSuccesseur)))
+				sendResponse(cmds[3], idSuccesseur+"-"+IPsuccesseur);
+			else if (idSuccesseur == hashToGet)
+				sendResponse(cmds[3], idSuccesseur+"-"+IPsuccesseur);
+			else if (idSuccesseur == hash)
+				sendResponse(cmds[3], hash+"-"+ip);
+			else
+				passToSuccessor(message);
+			break;
+
+
+			/*
+			 * Clients messages 
+			 */
+		case "transmit" :
+			int hashTo = PeersUtility.safeParseInt(cmds[1]);
+			if(hashTo != hash) {
+				if(idSuccesseur > hash && idSuccesseur > hashTo)
+					passToSuccessor(message);
+				else if (idSuccesseur < hash && !(hashTo > hash || hashTo > 0 && hashTo < idSuccesseur))
+					passToSuccessor(message);
+				else
+					sendResponse(cmds[3], "error : no hash corresponding");
+			} else 
+			{
+				System.out.println("You received a message from "+cmds[2]+" : ");
+				System.out.println("\t"+cmds[4]);
+				sendResponse(cmds[3], "Message well received");
+			}
+
+			break;
+
+		case "who" :
+			int hashfrom = Integer.valueOf(cmds[1]);
+			if(hashfrom != hash) 
+				passToSuccessor(message+" - "+hash);
+			else 
+				System.out.println("list of hashes  : "+cmds[2]);
+
+			break;
+
+		default:
+			System.err.println("Received message '"+message+"', cannot treat");
+			break;
+		}
+	}
+
+
+	private static boolean ClientJoining(String IPKnown) {
+		boolean ret = true;
+
+		System.out.println("Looking for Successor...");
+		String[] result = (getResponseIP(IPKnown, "getSucc:"+hash+":"+hash+":"+ip)).split("-");
+		idSuccesseur = PeersUtility.safeParseInt(result[0]);
+		IPsuccesseur = result[1];
+		finger.put(idSuccesseur, IPsuccesseur);
+		finger_moniteur.add(hash+":"+idSuccesseur+":"+IPsuccesseur);
+		if(idSuccesseur == hash){
+			System.err.println("hash already taken");
+			ret = false;
+		}
+
+
+		System.out.println("Getting Predecessor...");
+		result = null;
+
+		//idSuccesseur ne sert à rien dans cette envois, mais c'est par soucis de respecter le format des messages
+		result = (getResponseIP(IPsuccesseur, "addme:"+idSuccesseur+":"+hash+":"+ip)).split("-");
+		idPredecesseur = PeersUtility.safeParseInt(result[0]);
+		IPpredecesseur = result[1];
+
+		System.out.println("Updating Predecessor...");
+		sendToIP(IPpredecesseur, "newSucc:"+idSuccesseur+":"+hash+":"+ip);
+
+		return ret;
+	}
+
+
+
+
+
+	private static void sendResponse(String IP, String message) {
+		PeersUtility.sendToIP(IP, message, 2017);
+	}
+	private static void sendToIP(String IP, String message) {
+		PeersUtility.sendToIP(IP, message, 2016);
+	}
+	private static void passToSuccessor(String message) {
+		PeersUtility.sendToIP(IPsuccesseur, message, 2016);
+	}
+
+
+
+
+	private static String getResponse(String command, int hashTo, String message) {
+		String send = command+":"+hashTo+":"+hash+":"+ip+":"+message;
+		return PeersUtility.getResponseIP(IPsuccesseur, send);
+	}
+
+	private static String getResponseIP(String IP, String message) {
+		return PeersUtility.getResponseIP(IP, message);
+	}
+
+
+
+
+
+
+	public static void refreshFinger() {
+		System.out.println("Refreshing Routing Table");
+		finger.clear();
+		finger_moniteur.clear();
+
+		int max = (int) (Math.log(SERVER_SIZE) / Math.log(2));
+		int puissance = 0;
+		for (int i = 0; i <= max; i++) {
+			puissance = (hash + (int)Math.pow(2, i)) % SERVER_SIZE;
+			String[] result = (getResponseIP("localhost", "getSucc:"+puissance+":"+hash+":"+ip)).split("-");
+			int hp = PeersUtility.safeParseInt(result[0]);
+
+			finger.put(hp, result[1]);
+			finger_moniteur.add(puissance+":"+hp+":"+result[1]);
+
+		}
+		for (String key : finger_moniteur) {
+			System.out.println(key);
+		}
+	}
+
+
+
+
+
+
+	protected static void serverListener() {
+		try(ServerSocket ss = new ServerSocket(2016);){
+
+			System.out.println("Start listening server");
+			while(true) {
+				try(Socket cs = ss.accept();
+						BufferedReader d = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+						PrintStream os = new PrintStream (cs.getOutputStream(), true);
+						) 
+				{
+					String message = "";
+					while((message = d.readLine())!= null) 
+					{
+						TreatMessage(message, os, d, cs);
+
+					}
+				}
+				catch (IOException e) {
+					System.err.println("Listening server IO Erreur");
+				}
+			}
+		}catch (IOException e) {System.err.println("erreur starting listening server"); return;}
+
+	}
+
+	protected static void monitorListener() {
+		try(ServerSocket ss = new ServerSocket(8002);){
+			while(true) {
+				try(Socket cs = ss.accept();
+						BufferedReader d = new BufferedReader(new InputStreamReader(cs.getInputStream()));
+						PrintStream os = new PrintStream (cs.getOutputStream(), true);
+						) 
+				{
+					System.out.println("Starting sending routing table");
+					d.readLine();
+					for (String key : finger_moniteur) {
+						os.println(key);
+					}
+					os.println("end");
+				}
+				catch (IOException e) {
+					System.err.println("Monitor server IO Erreur");
+				}
+			}
+		}catch (IOException e) {System.err.println("erreur starting monitor server"); return;}
+	}
+
+	private static void scanCommandLine() {
 		try (Scanner scan = new Scanner(System.in)) {
 			while(true) {
 				String mess = scan.nextLine();
 				switch (mess) {
-				case "sendTo":
-					System.out.println("send to Who ?");
-					mess += ":"+scan.nextLine();
-					mess += ":"+hash;
-					System.out.println("What'd you wanna send ?");
-					mess += ":"+(scan.nextLine().replaceAll(":", " "));
-					sendToSuccessor("transfert:"+mess);
+				case "info":
+					System.out.println("IP Succ : " +IPsuccesseur);
+					System.out.println("Id Succ : " +idSuccesseur);
+					System.out.println("IP Pred : " +IPpredecesseur);
+					System.out.println("Id Pred : " +idPredecesseur);
 					break;
 
-				case "who":
-					mess += ":"+hash;
-					mess += ":"+hash+";";
-					sendToSuccessor(mess);
+				case "transmit" :
+					System.out.println("Hash to send to : ");
+					int hashTo = scan.nextInt();
+					scan.nextLine();
+					System.out.println("What to send : ");
+					String message = scan.nextLine();
+					System.out.println(getResponse("transmit", hashTo, message));
 					break;
-
-				case "refresh":
-					mess += ":"+hash+":";
-					sendToSuccessor(mess);
+				case "who" :
+					passToSuccessor("who:"+hash+":"+hash);
 					break;
-
-
-				case "bye":
-					mess += ":"+idPredecesseur;
-					mess += ":"+IPpredecesseur;
-					sendToSuccessor(mess);
+				case "refresh" :
+					refreshFinger();
 					break;
-
 				default:
-					if(mess.matches("sendTo:(\\d)+:"+hash+":[^:]*"))
-						sendToSuccessor("transfert:"+mess);
-					else {
-						System.out.println("usage :");
-						System.out.println("sendTo:[HashTo]:[YourHash]:[message]");
-						System.out.println("or just sendTo, and follow the instructions");
-					}
 					break;
 				}
 
+
 			}
-		}
-
-
-	}
-
-
-	private static void TreatMessage(String message, PrintStream os, BufferedReader d, Socket cs) {
-		String[] cmds = message.split(":");
-		switch (cmds[0]) {
-		case "init" :
-			switch (cmds[1]) {
-			case "addme_pls" :
-				AddToNetwork(os, d, cs);
-				break;
-
-			case "Hey Im new" :
-				setSuccessor(os, d, cs);
-				break;
-			default:
-				break;
-			}
-			break;
-
-			//transfert ::  transfert:[typeTransfert]:[hashTo]:[hashFrom]:[message]
-		case "transfert" :
-			System.out.println(message);
-			int hashTo = Integer.valueOf(cmds[2]);
-			if(hashTo != hash) {
-				if(idSuccesseur > hash) {
-					if(idSuccesseur > hashTo) {
-						sendToSuccessor("transfert:ERRORsendTo:"+cmds[3]+":"+hash+":the peers "+cmds[2]+" was not found");
-					} else {
-						sendToSuccessor(message);
-					}
-				} else {
-					if(hashTo > hash) {
-						sendToSuccessor("transfert:ERRORsendTo:"+cmds[3]+":"+hash+":the peers "+cmds[2]+" was not found");
-					}else if (hashTo > 0 && hashTo < idSuccesseur) {
-						sendToSuccessor("transfert:ERRORsendTo:"+cmds[3]+":"+hash+":the peers "+cmds[2]+" was not found");
-					} else {
-						sendToSuccessor(message);
-					}
-				}
-			} else {
-				if(cmds[1].equals("sendTo")) 
-				{
-					System.out.println("Received message from "+cmds[3]+" : "+cmds[4]);
-					sendToSuccessor("transfert:sendToWellReceived:"+cmds[3]+":"+hash);
-				} 
-				if(cmds[1].equals("sendToWellReceived")) 
-				{
-					System.out.println("The message to "+cmds[3]+" was well received.");
-				} 
-				if(cmds[1].equals("ERRORsendTo")) 
-				{
-					System.err.println(cmds[4]);
-				}
-			}
-
-			break;
-
-		case "who":
-			int hashfrom = Integer.valueOf(cmds[1]);
-			if(hashfrom != hash) {
-				sendToSuccessor(message+hash+";");
-			} else {
-				System.out.println("list of hashes  :"+cmds[2]);
-			}
-			break;
-
-		case "refresh":
-			System.out.println(message);
-			int hashfrom2 = Integer.valueOf(cmds[1]);
-			if(hashfrom2 != hash) {
-				sendToSuccessor(message+hash+"-"+ip+";");
-			} else {
-				refreshFinger(cmds[2]);
-			}
-			break;
-		case "bye":
-			idPredecesseur = Integer.valueOf(cmds[1]);
-			IPpredecesseur = cmds[2];
-			sendToPredecessor("newSuccessor:"+hash+":"+ip);
-			break;
-
-
-		case "newSuccessor":
-			idSuccesseur = Integer.valueOf(cmds[1]);
-			IPsuccesseur = cmds[2];
-			finger.put(idSuccesseur, IPsuccesseur);
-			break;
-
-
-		default:
-			//System.out.println(message);
-			break;
 		}
 	}
-
-
-	private static void AddToNetwork(PrintStream os, BufferedReader d, Socket cs) {
-		System.out.println("Adding a peer to the network");
-		os.println("K.");
-		os.println(hash);
-		os.println(IPpredecesseur);
-		os.println(idPredecesseur);
-
-
-
-		IPpredecesseur = cs.getInetAddress().getHostAddress();
-		try {
-			idPredecesseur = Integer.valueOf(d.readLine());
-		} catch (IOException e) {
-			System.err.println("fuck he's ded");
-		}
-		System.out.println("Peer added to the network");
-	}
-
-	private static void setSuccessor(PrintStream os, BufferedReader d, Socket cs) {
-		System.out.println("Setting new successor");
-		IPsuccesseur = cs.getInetAddress().getHostAddress();
-		os.println("hash pls?");
-		try {
-			idSuccesseur = Integer.valueOf(d.readLine());
-		} catch (IOException e) {
-			System.err.println("fuck he's ded");
-		}
-		finger.put(idSuccesseur, IPsuccesseur);
-		System.out.println("Successor added");
-	}
-
-
-	private static void ClientJoining(String IPKnown) {
-		System.out.println("Trying to join the server");
-
-		IPsuccesseur = IPKnown;
-		
-		try 
-		(		Socket clientPresent = new Socket(IPKnown, 2016);
-				BufferedReader in = new BufferedReader(new InputStreamReader (clientPresent.getInputStream()));
-				PrintStream out = new PrintStream (clientPresent.getOutputStream(), true);)
-				{
-
-			out.println("init:addme_pls");
-			if(in.readLine().equals("K.")) {
-				idSuccesseur = Integer.valueOf(in.readLine());
-				IPpredecesseur = in.readLine();
-				idPredecesseur = Integer.valueOf(in.readLine());
-				finger.put(idSuccesseur, IPsuccesseur);
-				out.println(hash);
-			} else{
-				System.out.println("wut ?!");
-			}
-
-				} 	
-		catch(UnknownHostException e) {System.err.println("unknown host"); return;}
-		catch ( IOException e ) {System.err.println("I/O error joining known host"); return;}
-
-
-		try 
-		(		Socket pred = new Socket(IPpredecesseur, 2016);
-				BufferedReader in = new BufferedReader(new InputStreamReader (pred.getInputStream()));
-				PrintStream out = new PrintStream (pred.getOutputStream(), true);)
-				{
-
-			out.println("init:Hey Im new");
-			if(in.readLine().equals("hash pls?")) {
-				out.println(hash);	
-			} else{
-				System.out.println("wut ?!");
-			}
-				} 	
-		catch(UnknownHostException e) {System.err.println("unknown host"); return;}
-		catch ( IOException e ) {System.err.println("I/O error joining predecessor host"); return;}
-	}
-
-
-	public static void sendToSuccessor(String message) {
-		try(Socket sc = new Socket(IPsuccesseur, 2016);
-				PrintStream os = new PrintStream (sc.getOutputStream(), true);) {
-			os.println(message);
-		}
-		catch (IOException e) {
-			System.err.println("Sending server IO Erreur");
-		}
-	}
-	public static void sendToPredecessor(String message) {
-		try(Socket sc = new Socket(IPpredecesseur, 2016);
-				PrintStream os = new PrintStream (sc.getOutputStream(), true);) {
-			os.println(message);
-		}
-		catch (IOException e) {
-			System.err.println("Sending server IO Erreur");
-		}
-	}
-	
-	
-	public static void refreshFinger(String message) {
-		System.out.println("Refreshing Routing Table");
-		finger.clear();
-		
-		String[] hashIP = message.split(";");
-		
-		ArrayList<String> allHash = new ArrayList<String>();
-		ArrayList<String> allIP = new ArrayList<String>();
-		for (String  string: hashIP) {
-			String[] splitted = string.split("-");
-			allHash.add(splitted[0]);
-			allIP.add( splitted[1]);
-		}
-		
-		
-		
-		int max = (int) Math.log(SERVER_SIZE);
-		int puissance = 0;
-		for (int i = 1; i < max; i++) {
-			boolean endLoop = false;
-			puissance = (hash + (int)Math.pow(2, i));
-			endLoop = puissance > SERVER_SIZE;
-			int hashI = 0;
-			int index = -1;
-			for (String hashS : allHash) {
-				System.out.println(hashS);
-				index++;
-				hashI = Integer.valueOf(hashS);
-				if(!endLoop) {
-					if (hashI >= puissance) {
-						
-						
-						if(!finger.containsKey(hashI))
-							finger.put(hashI, allIP.get(index));
-						break;
-					}
-				} else {
-					if(hashI < hash) {
-						if (hashI + SERVER_SIZE >= puissance) {
-							
-							
-							if(!finger.containsKey(hashI))
-								finger.put(hashI, allIP.get(index));
-							break;
-						}
-					}
-				}
-			}
-			
-			for (int key : finger.keySet()) {
-				System.out.print(key+" : ");
-				System.out.println(finger.get(key));
-			}
-			
-		}
-	}
-
-
 
 }
